b800165a78f7476ee3c9f5e3cd4fed801cd03b6b
Gerber <dv15lgr@ad.cs.umu.se>
2019-09-08 17:15:45 +0200

    robot radian angle to geonorth degree converter

--- TestRobot3.py
+++ TestRobot3.py
@@ -1,6 +1,7 @@
 from Robot import *
 from Path import *
 from Calibrate import *
+from AngleConverter import *
 
 # load a path file
 p = Path("Path-around-table.json")
@@ -11,11 +12,12 @@ print("First point = " + str(path[0]['X']) + ", " + str(path[0]['Y']))
 
 # make a robot to move around
 robot = Robot()
+converter = AngleConverter()
 calibration = Calibrate(robot)
 calibration.calibrate()
-print(robot.getHeading())
+print(converter.convertToDegree(robot.getHeading()))
 robot.setMotion(0,0.104)
 sleep(60)
 robot.setMotion(0,0)
-print(robot.getHeading())
+print(converter.convertToDegree(robot.getHeading()))
 
